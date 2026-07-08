afa9cfb3e2af583bbfd3d71875b175d754b5ad70
shivam kumar <123shivamkumar123456@gmail.com>
2026-07-09 02:52:46 +0530

    modified hardcoded url in index.js

--- backend/index.js
+++ backend/index.js
@@ -17,12 +17,12 @@ import { socketHandler } from "./socket.js"
 const app=express()
 const server=http.createServer(app)
 
-const io=new Server(server,{
-   cors:{
-    origin:"http://localhost:5173",
-    credentials:true,
-    methods:['POST','GET']
-}
+const io = new Server(server,{
+    cors:{
+        origin: process.env.FRONTEND_URL,
+        credentials:true,
+        methods:["GET","POST"]
+    }
 })
 
 app.set("io",io)
@@ -31,8 +31,8 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: process.env.FRONTEND_URL,
+    credentials: true
 }))
 app.use(express.json())
 app.use(cookieParser())
@@ -43,8 +43,13 @@ app.use("/api/item",itemRouter)
 app.use("/api/order",orderRouter)
 
 socketHandler(io)
-server.listen(port,()=>{
-    connectDb()
-    console.log(`server started at ${port}`)
-})
+connectDb()
+    .then(() => {
+        server.listen(port, () => {
+            console.log(`Server running on ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
